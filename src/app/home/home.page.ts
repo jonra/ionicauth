75e9d1f53834b5ee3d8048c6cc602cf7520cb380
CodingApp <44866144+CodingApp@users.noreply.github.com>
2019-08-03 20:47:01 +0500

    Update home.page.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,29 +4,26 @@ import { AngularFireAuth } from '@angular/fire/auth';
 import { auth } from 'firebase/app';
 import { Router } from '@angular/router';
 
-
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss']
 })
 export class HomePage {
-  constructor( private aut: AngularFireAuth, private router: Router) {
- //   this.profileInfo();
+  constructor( private rout: Router, private aut: AngularFireAuth, private router: Router) {
     this.aut.authState
-        .subscribe(
-            user => {
-              if (user) {
-                this.router.navigateByUrl('home');
-              } else {
-                this.router.navigateByUrl('/login');
-              }
-            },
-            () => {
-              this.router.navigateByUrl('/login');
-            }
-        );
+    .subscribe(
+      user => {
+        if (user) {
+          this.rout.navigateByUrl('home');
+        } else {
+          this.rout.navigateByUrl('/login');
+        }
+      },
+      () => {
+        this.rout.navigateByUrl('/login');
+      }
+    );
   }
 
   async signOut() {
